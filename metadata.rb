a7ad29b7a02b8dc2b3e45388cb49fd8e3d222a15
Seth Kingry <s.kingry@modcloth.com>
2012-12-14 11:07:45 -0800

    Updated cookbook to have parity with what exists in reference cookbook in platform infrastructure branch

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,11 @@
-name              "openssh"
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs openssh"
-version           "1.1.2"
+version           "1.2.2"
 
 recipe "openssh", "Installs openssh"
 
-%w{ redhat centos fedora ubuntu debian arch scientific }.each do |os|
+%w{ redhat centos fedora ubuntu debian arch scientific smartos }.each do |os|
   supports os
 end
